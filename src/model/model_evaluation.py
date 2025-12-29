3a51ee5669f4eaf25512fe2ab4f6cbfbe5e04591
shikhagupta1 <shikgupta@cs.stonybrook.edu>
2025-12-28 23:37:16 -0800

    dvc pipeline

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -14,25 +14,25 @@ from src.logger import logging
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "bansal1600"
+repo_name = "classification-sentiment-mlops"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/bansal1600/classification-sentiment-mlops.mlflow')
-dagshub.init(repo_owner='bansal1600', repo_name='classification-sentiment-mlops', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/bansal1600/classification-sentiment-mlops.mlflow')
+# dagshub.init(repo_owner='bansal1600', repo_name='classification-sentiment-mlops', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
